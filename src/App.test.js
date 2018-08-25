abfbd7561ff13bbf3100a56d1fb3dc993186b7ca
Jose Leal <jose.r.s.leal@gmail.com>
2018-08-25 17:59:12 +0300

    tests pass with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+	const comp = shallow(<App />);
+	expect(comp.exists()).toEqual(true);
+	/*   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.unmountComponentAtNode(div); */
 });
